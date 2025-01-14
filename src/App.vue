f3a3d137e00f361b0813de47d66d9d1b405d62f7
brad preston <bap5393@gmail.com>
2025-01-14 17:35:38 -0500

    Update App.vue to show title and clean styling

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <main class="container mx-auto">
-    <header class="prose">
-      <h1>Cleaned App.vue</h1>
+  <main class="container mx-auto py-10">
+    <header class="prose mx-auto">
+      <h1 class="text-center">Phase 10 Scoresheet</h1>
     </header>
   </main>
 </template>
